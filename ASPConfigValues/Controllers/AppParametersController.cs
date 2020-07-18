d59211ee8466d3737b123023f16f0a89b3d14e36
jpd21122012 <slipknot_jpd@hotmail.com>
2020-07-17 21:08:15 -0500

    Add project files.

---
+++ ASPConfigValues/Controllers/AppParametersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASPConfigValues.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace ASPConfigValues.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppParametersController : ControllerBase
+    {
+        private ApplicationParameters applicationParameters;
+        private ApplicationParameters applicationParametersSnapshot;
+        public AppParametersController(IOptions<ApplicationParameters> options, IOptionsSnapshot<ApplicationParameters> optionsSnap)
+        {
+            applicationParameters = options.Value;
+            applicationParametersSnapshot = optionsSnap.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            StringBuilder response = new StringBuilder();
+            response.AppendLine("IOptions ==>");
+            response.AppendLine(JsonConvert.SerializeObject(applicationParameters));
+            response.AppendLine("");
+            response.AppendLine("IOptionsSnapshot ==>");
+            response.AppendLine(JsonConvert.SerializeObject(applicationParametersSnapshot));
+            return Content(response.ToString());
+        }
+    }
+}
